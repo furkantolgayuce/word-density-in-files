6e582c4bb06b32e2c00564a53fbaab0e2d5a2b69
furkantolgayuce <yucefurkantolga@gmail.com>
2019-06-11 02:34:18 +0300

    Update quikstart.py

--- quikstart.py
+++ quikstart.py
@@ -2,17 +2,22 @@ from util import word_density
 
 # File PATH
 file = "example_file/Game.of.Thrones.S08E06.srt"
+print("{}\nYolundaki dosya analiz ediliyor..\n".format(file))
 
 # Class
 word_density = word_density(file)
+print("word_density sınıfı oluşturuldu.\n")
 
 # DataFrame
 df = word_density.get_df()
+print("word_density.get_df() ile DataFrame oluşturuldu.\nDataFrame içerisinde dosya içerisinde geçen kelimeler ve tekrar sayıları var.\n")
 
+print("İlk 5 Satır.\n")
 # let's take a quick look
 print(df.head())
 
 
+input("Grafiği Çizdirmek için bir tuşa basın..")
 # Make Chart (Default:25)
 word_density.get_chart()
 
